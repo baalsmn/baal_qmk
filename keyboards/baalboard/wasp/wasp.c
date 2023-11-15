c6264bcf93142010ac3e215fc4dc6129a5a953e0
baalsmn <baalintsimon@gmail.com>
2023-11-15 13:48:48 +0100

    init
    cd

---
+++ keyboards/baalboard/wasp/wasp.c
@@ -0,0 +1,14 @@
+/*
+This file is where you can find custom code for the keyboard.
+
+This is where you will write custom code for your keyboard. Typically you will write code to initialize and interface with the hardware in your keyboard. If your keyboard consists of only a key matrix with no LEDs, speakers, or other auxiliary hardware this file can be blank.
+
+The following functions are typically defined in this file:
+
+void matrix_init_kb(void)
+void matrix_scan_kb(void)
+bool process_record_kb(uint16_t keycode, keyrecord_t *record)
+bool led_update_kb(led_t led_state)
+
+
+*/
